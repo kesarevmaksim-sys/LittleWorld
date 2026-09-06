e9f04dd7efb86b4f1b25da0be9033ace350903dc
Kesarev Maks <kesarevmaksim@gmail.com>
2026-09-06 22:02:33 +0300

    Initial Commit

---
+++ LittleWorld/LittleWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleWorldApp.swift
+//  LittleWorld
+//
+//  Created by Макс Кесарев on 06.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct LittleWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
